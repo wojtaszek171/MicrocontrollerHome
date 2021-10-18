1f8d6396a16bc3c8ff47968872df8d7a0918a12f
Pawel Wojtaszko <pawelwojtaszko@o2.pl>
2021-10-18 19:03:26 +0200

    Added esp8266 with temperature and humidity sensors implementation

---
+++ ESP3/ESP3/ESP3.ino
@@ -0,0 +1,65 @@
+/*********
+  Complete project details at https://randomnerdtutorials.com  
+*********/
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include "DHTesp.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#define SEALEVELPRESSURE_HPA (1013.25)
+#define DHTPin D4
+
+DHTesp dht;
+WiFiClient wifi;
+
+String microcontrollerId = String("room3"); //ENTER ROOM id SSID
+/* Enter ssid & password of your WiFi inside double quotes */
+const char *ssid = ""; //ENTER WIFI SSID
+const char *password = ""; //ENTER WIFI PASSWORD
+String serverName = ""; //PUT YOUR API DOMAIN
+String apiKeyValue = ""; //ENTER API KEY
+const long sensorFetchInterval = 5000;
+unsigned long previousMillisSensor = 0;
+
+unsigned long delayTime;
+
+void setSensor(String name, float value)
+{
+  HTTPClient http;
+  http.begin(wifi, serverName + "setSensor.php");
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  String httpRequestData = "api_key=" + apiKeyValue + "&name=" + name + "&value=" + value + "";
+  int httpCode = http.POST(httpRequestData);
+  if (httpCode <= 0)
+  {
+    Serial.print("Error code: ");
+    Serial.println(httpCode);
+  }
+}
+
+void readSensors() {
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    setSensor(microcontrollerId + String("Humidity"), dht.getHumidity());
+    setSensor(microcontrollerId + String("Temp"), dht.getTemperature());
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  dht.setup(DHTPin, DHTesp::DHT22);
+  delayTime = 1000;
+  WiFi.begin(ssid, password); /* connect to WiFi */
+}
+
+void loop() { 
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillisSensor >= sensorFetchInterval)
+  {
+    previousMillisSensor = currentMillis;
+    readSensors();
+  }
+}
